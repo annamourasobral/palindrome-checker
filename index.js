bcae5ff5db8ca5001f943eb8dccef72adea6f0f7
Anna Moura Sobral <annamourasobral@gmail.com>
2022-04-23 20:25:06 +0200

    completing challenge

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
 */
 
 function palindrome(str) {
-    return true;
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
   }
-  
-  palindrome("eye");
+  return true;
+ }
+
+palindrome("eye");
